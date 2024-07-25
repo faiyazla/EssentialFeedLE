51ce4c0bdf4ede58e55a4b9ad3879fdd8729ee89
faiyazla <fyizahmed@gmail.com>
2024-07-25 10:04:10 +0545

    Create the FeedItem model

---
+++ EssentialFeedLE/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeedLE
+//
+//  Created by Faiyaz Ahmed on 25/07/2024.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
