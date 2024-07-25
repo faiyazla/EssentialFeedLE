8a5a1b93aee5baf3cabeaf7c944a39389dc7af03
faiyazla <fyizahmed@gmail.com>
2024-07-25 10:01:31 +0545

    Initial Commit

---
+++ EssentialFeedLE/EssentialFeedLE.h
@@ -0,0 +1,18 @@
+//
+//  EssentialFeedLE.h
+//  EssentialFeedLE
+//
+//  Created by Faiyaz Ahmed on 25/07/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialFeedLE.
+FOUNDATION_EXPORT double EssentialFeedLEVersionNumber;
+
+//! Project version string for EssentialFeedLE.
+FOUNDATION_EXPORT const unsigned char EssentialFeedLEVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialFeedLE/PublicHeader.h>
+
+
