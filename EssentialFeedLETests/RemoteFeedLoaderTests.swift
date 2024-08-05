77e1abcc7f878926f0cf6fa312fe03e38a299e6a
faiyazla <fyizahmed@gmail.com>
2024-08-05 10:13:41 +0545

    Move test logic to a separate type `HTTPClientSpy`

--- EssentialFeedLETests/RemoteFeedLoaderTests.swift
+++ EssentialFeedLETests/RemoteFeedLoaderTests.swift
@@ -10,57 +10,42 @@ import XCTest
 class RemoteFeedLoader {
     
     func load() {
-        HTTPClient.shared.requestURL = URL(string: "https://a-given-url.com")
+        HTTPClient.shared.get(from: URL(string: "https://a-url.com")!)
     }
 }
 class HTTPClient {
     
-    static let shared = HTTPClient()
+    static var shared = HTTPClient()
     
-    private init() {}
-    
-    var requestURL: URL?
+    func get(from url: URL) {}
 }
-
+class HTTPClientSpy: HTTPClient {
+    var requestURL: URL?
+    
+    override func get(from url: URL) {
+        requestURL = url
+    }
+}
 final class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
         _ = RemoteFeedLoader()
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
       
         
         XCTAssertNil(client.requestURL)
     }
     
     func test_load_requestDataFromURL() {
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         let sut = RemoteFeedLoader()
         sut.load()
         
         XCTAssertNotNil(client.requestURL)
     }
     
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
 
 }
