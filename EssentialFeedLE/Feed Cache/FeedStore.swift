e387c9c790e72c16d49241a58d833ee515c7ce4c
faiyazla <fyizahmed@gmail.com>
2025-02-17 15:14:50 +0545

    Move `FeedStore` to its own file

---
+++ EssentialFeedLE/Feed Cache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  EssentialFeedLE
+//
+//  Created by Faiyaz Ahmed on 17/02/2025.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
