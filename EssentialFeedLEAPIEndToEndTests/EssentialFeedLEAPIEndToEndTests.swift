328b136be0f495504f0e905fe0881fb6b0b19d17
faiyazla <fyizahmed@gmail.com>
2025-02-18 11:17:59 +0545

    Fix naming error from `imageURL` to `url`

--- EssentialFeedLEAPIEndToEndTests/EssentialFeedLEAPIEndToEndTests.swift
+++ EssentialFeedLEAPIEndToEndTests/EssentialFeedLEAPIEndToEndTests.swift
@@ -54,7 +54,7 @@ final class EssentialFeedLEAPIEndToEndTests: XCTestCase {
             id: id(at: index),
             description: description(at: index),
             location: location(at: index),
-            imageURL: imageURL(at: index))
+            url: imageURL(at: index))
     }
     
     private func id(at index: Int) -> UUID {
