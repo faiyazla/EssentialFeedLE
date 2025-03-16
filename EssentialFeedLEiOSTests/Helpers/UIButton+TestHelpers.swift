3a9e88e3750e491b6691ac17954b863b0a15297a
faiyazla <fyizahmed@gmail.com>
2025-03-16 10:57:05 +0545

    Changed access modifier to `internal`

---
+++ EssentialFeedLEiOSTests/Helpers/UIButton+TestHelpers.swift
@@ -0,0 +1,8 @@
+//
+//  UIButton+TestHelpers.swift
+//  EssentialFeedLEiOSTests
+//
+//  Created by Faiyaz Ahmed on 16/03/2025.
+//
+
+import Foundation
--- EssentialFeedLEiOSTests/Helpers/UIRefreshControl+TestHelpers.swift
+++ EssentialFeedLEiOSTests/Helpers/UIRefreshControl+TestHelpers.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-private extension UIRefreshControl {
+extension UIRefreshControl {
     func simulatePullToRefresh() {
         allTargets.forEach { target in
             actions(forTarget: target, forControlEvent: .valueChanged)?.forEach {
