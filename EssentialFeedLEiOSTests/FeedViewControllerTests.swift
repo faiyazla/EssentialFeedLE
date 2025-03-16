2fba5670c54f493f4eb610a1da7ed00edaee6abb
faiyazla <fyizahmed@gmail.com>
2025-03-16 10:52:12 +0545

    Extract UIImage test helper to a separate file

--- EssentialFeedLEiOSTests/FeedViewControllerTests.swift
+++ EssentialFeedLEiOSTests/FeedViewControllerTests.swift
@@ -461,18 +461,7 @@ private extension FeedImageCell {
         feedImageRetryButton.simulateTap()
     }
 }
-extension UIImage {
-    static func make(withColor color: UIColor) -> UIImage {
-        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
-        let format = UIGraphicsImageRendererFormat()
-        format.scale = 1
-        
-        return UIGraphicsImageRenderer(size: rect.size, format: format).image { rendererContext in
-            color.setFill()
-            rendererContext.fill(rect)
-        }
-    }
-}
+
 private extension UIButton {
     func simulateTap() {
         allTargets.forEach { target in
