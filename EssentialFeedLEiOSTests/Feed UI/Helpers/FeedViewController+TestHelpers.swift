cfe9bfe3620b6070125fe29eeed6075375057ec8
faiyazla <fyizahmed@gmail.com>
2025-03-17 11:31:32 +0545

    setting the refreshController to the FakeRefreshControl so as to fix the test `test_loadingFeedIndicator_isVisibleWhileLoadingFeed` for iOS17

--- EssentialFeedLEiOSTests/Feed UI/Helpers/FeedViewController+TestHelpers.swift
+++ EssentialFeedLEiOSTests/Feed UI/Helpers/FeedViewController+TestHelpers.swift
@@ -68,6 +68,7 @@ extension FeedViewController {
             }
         }
         refreshControl = fake
+        refreshController?.view = fake
     }
     
     func simulateFeedImageViewNearVisible(at row: Int) {
