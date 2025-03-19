c09f9360d435bbfff679a93041b45aee998967a6
faiyazla <fyizahmed@gmail.com>
2025-03-19 10:52:48 +0545

    Move ViewModels to separate files

---
+++ EssentialFeedLEiOS/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeedLEiOS
+//
+//  Created by Faiyaz Ahmed on 19/03/2025.
+//
+
+import Foundation
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssentialFeedLEiOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeedLE
+//
+//  Created by Faiyaz Ahmed on 19/03/2025.
+//
+
+import EssentialFeedLE
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
