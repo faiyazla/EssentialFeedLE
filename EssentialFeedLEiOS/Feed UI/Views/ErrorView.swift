2066848032322027bf03918801cf06b6942f8aa6
faiyazla <fyizahmed@gmail.com>
2025-03-25 16:03:03 +0545

    Move `ErrorView` to separate file

---
+++ EssentialFeedLEiOS/Feed UI/Views/ErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorView.swift
+//  EssentialFeedLE
+//
+//  Created by Faiyaz Ahmed on 25/03/2025.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
