f4892d0bb88a85e689a2901339cebff0dbb85c2d
faiyazla <fyizahmed@gmail.com>
2025-03-26 14:59:26 +0545

    Move `FeedLoadingViewModel` to separate file

---
+++ EssentialFeedLE/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeedLE
+//
+//  Created by Faiyaz Ahmed on 26/03/2025.
+//
+
+import Foundation
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
