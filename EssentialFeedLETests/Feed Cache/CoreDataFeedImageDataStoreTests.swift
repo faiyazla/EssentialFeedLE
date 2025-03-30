cdd4c4e34925d30f88b6595090bf2f769eaec35d
faiyazla <fyizahmed@gmail.com>
2025-03-30 17:31:19 +0545

    CoreDataFeedStore.retrieveImageData delivers stored data when there's an image with a matching URL in the store. Also, added FeedStore2 via Editor -> Add Model Version and set it as the current version

--- EssentialFeedLETests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
+++ EssentialFeedLETests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
@@ -8,18 +8,6 @@
 import XCTest
 import EssentialFeedLE
 
-extension CoreDataFeedStore: @retroactive FeedImageDataStore {
-    
-    public func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
-        
-    }
-    
-    public func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
-        completion(.success(.none))
-    }
-    
-}
-
 class CoreDataFeedImageDataStoreTests: XCTestCase {
     
     func test_retrieveImageData_deliversNotFoundWhenEmpty() {
@@ -38,6 +26,16 @@ class CoreDataFeedImageDataStoreTests: XCTestCase {
         expect(sut, toCompleteRetrievalWith: notFound(), for: nonMatchingURL)
     }
     
+    func test_retrieveImageData_deliversFoundDataWhenThereIsAStoredImageDataMatchingURL() {
+        let sut = makeSUT()
+        let storedData = anyData()
+        let matchingURL = URL(string: "http://a-url.com")!
+        
+        insert(storedData, for: matchingURL, into: sut)
+        
+        expect(sut, toCompleteRetrievalWith: found(storedData), for: matchingURL)
+    }
+    
     // - MARK: Helpers
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> CoreDataFeedStore {
@@ -46,6 +44,10 @@ class CoreDataFeedImageDataStoreTests: XCTestCase {
         let sut = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
+    }
+    
+    private func found(_ data: Data) -> FeedImageDataStore.RetrievalResult {
+        return .success(data)
     }
     
     private func notFound() -> FeedImageDataStore.RetrievalResult {
