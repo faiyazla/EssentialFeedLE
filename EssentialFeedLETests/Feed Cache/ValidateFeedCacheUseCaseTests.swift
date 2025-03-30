777a331ab369aca9577584e63db73e9d4a64d9df
faiyazla <fyizahmed@gmail.com>
2025-03-30 18:11:45 +0545

    `LocalFeedLoader.validateCache` succeeds on non-expired cache

--- EssentialFeedLETests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
+++ EssentialFeedLETests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
@@ -79,6 +79,18 @@ final class ValidateFeedCacheUseCaseTests: XCTestCase {
         })
     }
     
+    func test_validateCache_succeedsOnNonExpiredCache() {
+        let feed = uniqueImageFeed()
+        
+        let fixedCurrentDate = Date()
+        let nonExpiredTimestamp = fixedCurrentDate.minusFeedCacheMaxAge().adding(seconds: 1)
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+        
+        expect(sut, toCompleteWith: .success(()), when: {
+            store.completeRetrieval(with: feed.local, timestamp: nonExpiredTimestamp)
+        })
+    }
+    
     func test_validateCache_succeedsOnSuccessfulDeletionOfFailedRetrieval() {
         let (sut, store) = makeSUT()
         
