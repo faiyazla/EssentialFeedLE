7ad1c0545934919479866699ae340a828e15d1c3
faiyazla <fyizahmed@gmail.com>
2025-03-30 17:37:07 +0545

    Remove unnecessary return statements

--- EssentialFeedLE/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataLoader.swift
+++ EssentialFeedLE/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataLoader.swift
@@ -12,17 +12,17 @@ extension CoreDataFeedStore: FeedImageDataStore {
     public func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
         perform { context in
             completion(Result {
-                 let image = try ManagedFeedImage.first(with: url, in: context)
-                 image?.data = data
-                 try context.save()
-             })
+                let image = try ManagedFeedImage.first(with: url, in: context)
+                image?.data = data
+                try context.save()
+            })
         }
     }
     
     public func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
         perform { context in
             completion(Result {
-                return try ManagedFeedImage.first(with: url, in: context)?.data
+                try ManagedFeedImage.first(with: url, in: context)?.data
             })
         }
     }
--- EssentialFeedLE/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
+++ EssentialFeedLE/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -26,21 +26,21 @@ public final class CoreDataFeedStore: FeedStore {
     }
     
     public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-         perform { context in
-             completion(Result {
-                 let managedCache = try ManagedCache.newUniqueInstance(in: context)
-                 managedCache.timestamp = timestamp
-                 managedCache.feed = ManagedFeedImage.images(from: feed, in: context)
-                 try context.save()
-             })
-         }
-     }
-
+        perform { context in
+            completion(Result {
+                let managedCache = try ManagedCache.newUniqueInstance(in: context)
+                managedCache.timestamp = timestamp
+                managedCache.feed = ManagedFeedImage.images(from: feed, in: context)
+                try context.save()
+            })
+        }
+    }
+    
     public func retrieve(completion: @escaping RetrievalCompletion) {
         perform { context in
             completion(Result {
                 try ManagedCache.find(in: context).map {
-                    return CachedFeed(feed: $0.localFeed, timestamp: $0.timestamp)
+                    CachedFeed(feed: $0.localFeed, timestamp: $0.timestamp)
                 }
             })
         }
