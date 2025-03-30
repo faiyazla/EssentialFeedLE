1c799852e010630b6795fbd0f3b18bc5727b621c
faiyazla <fyizahmed@gmail.com>
2025-03-30 10:15:09 +0545

    RemoteFeedImageDataLoader requests data from URL twice on calling `loadImageDataFromURL` twice

--- EssentialFeedLETests/Feed API/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeedLETests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -25,19 +25,29 @@ final class RemoteFeedImageDataLoaderTests: XCTestCase {
     func test_init_doesNotRequestDataFromURL() {
         let (_, client) = makeSUT()
         
-        XCTAssertTrue(client.requestURLs.isEmpty)
+        XCTAssertTrue(client.requestedURLs.isEmpty)
     }
     
     func test_loadImageDataFromURL_requestsDataFromURL() {
         let url = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
-
+        
         sut.loadImageData(from: url) { _ in }
         
-        XCTAssertEqual(client.requestURLs, [url])
+        XCTAssertEqual(client.requestedURLs, [url])
     }
     
+    func test_loadImageDataFromURLTwice_requestsDataFromURLTwice() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.loadImageData(from: url) { _ in }
+        sut.loadImageData(from: url) { _ in }
+        
+        XCTAssertEqual(client.requestedURLs, [url, url])
+    }
     
+    // MARK: - Helpers
     private func makeSUT(url: URL = anyURL(), file: StaticString = #file, line: UInt = #line) -> (sut: RemoteFeedImageDataLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemoteFeedImageDataLoader(client: client)
@@ -47,10 +57,10 @@ final class RemoteFeedImageDataLoaderTests: XCTestCase {
     }
     
     private class HTTPClientSpy: HTTPClient {
-        var requestURLs = [URL]()
+        var requestedURLs = [URL]()
         
         func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
-            requestURLs.append(url)
+            requestedURLs.append(url)
         }
     }
 }
