141259a6814002bfb33181a163cb8ef36443c3ac
faiyazla <fyizahmed@gmail.com>
2025-04-15 15:46:34 +0545

    iOS17+ fix: To simulate refresh control to the test

--- EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
+++ EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
@@ -15,6 +15,8 @@ class FeedAcceptanceTests: XCTestCase {
     func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() {
         let feed = launch(httpClient: .online(response), store: .empty)
         
+        feed.simulateAppearance()
+        
         XCTAssertEqual(feed.numberOfRenderedFeedImageViews(), 2)
         XCTAssertEqual(feed.renderedFeedImageData(at: 0), makeImageData())
         XCTAssertEqual(feed.renderedFeedImageData(at: 1), makeImageData())
