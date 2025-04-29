1c538198194bfd0526b7fe8d997b39f7b6d7b4a2
faiyazla <fyizahmed@gmail.com>
2025-04-29 13:03:31 +0545

    Duplicate RemoteFeedLoader as RemoteImageCommentsLoader

---
+++ EssentialFeedLE/Feed API/ImageCommentsMapper.swift
@@ -0,0 +1,22 @@
+//
+//  ImageCommentsMapper.swift
+//  EssentialFeedLE
+//
+//  Created by Faiyaz Ahmed on 29/04/2025.
+//
+
+import Foundation
+
+final class ImageCommentsMapper {
+    private struct Root: Decodable {
+        let items: [RemoteFeedItem]
+    }
+    
+    static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteFeedItem] {
+        guard response.isOK, let root = try? JSONDecoder().decode(Root.self, from: data) else {
+            throw RemoteImageCommentsLoader.Error.invalidData
+        }
+        
+        return root.items
+    }
+}
