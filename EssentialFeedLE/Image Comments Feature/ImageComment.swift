cb1c630eb7407b1b57930dc57f21fab244ff4119
faiyazla <fyizahmed@gmail.com>
2025-04-29 13:09:30 +0545

    Add ImageComment data model

---
+++ EssentialFeedLE/Image Comments Feature/ImageComment.swift
@@ -0,0 +1,22 @@
+//
+//  ImageComment.swift
+//  EssentialFeedLE
+//
+//  Created by Faiyaz Ahmed on 29/04/2025.
+//
+
+import Foundation
+
+public struct ImageComment: Equatable {
+    public let id: UUID
+    public let message: String
+    public let createdAt: Date
+    public let username: String
+    
+    public init(id: UUID, message: String, createdAt: Date, username: String) {
+        self.id = id
+        self.message = message
+        self.createdAt = createdAt
+        self.username = username
+    }
+}
