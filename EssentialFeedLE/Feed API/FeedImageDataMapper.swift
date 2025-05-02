44edac6cf4a8158f5e77d8f5d8088026efac9d55
faiyazla <fyizahmed@gmail.com>
2025-05-02 17:45:09 +0545

    Test FeedImageDataMapper in isolation

---
+++ EssentialFeedLE/Feed API/FeedImageDataMapper.swift
@@ -0,0 +1,23 @@
+//
+//  FeedImageDataMapper.swift
+//  EssentialFeedLE
+//
+//  Created by Faiyaz Ahmed on 02/05/2025.
+//
+
+import Foundation
+
+public final class FeedImageDataMapper {
+    
+    public enum Error: Swift.Error {
+        case invalidData
+    }
+    
+    public static func map(_ data: Data, from response: HTTPURLResponse) throws -> Data {
+        guard response.isOK, !data.isEmpty else {
+            throw Error.invalidData
+        }
+        
+        return data
+    }
+}
