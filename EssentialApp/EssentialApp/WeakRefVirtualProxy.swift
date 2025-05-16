a472956bcb33059d6710106b0a938a1e03e2ca16
faiyazla <fyizahmed@gmail.com>
2025-05-16 10:31:09 +0545

    Remove unused FeedImagePresenter logic

--- EssentialApp/EssentialApp/WeakRefVirtualProxy.swift
+++ EssentialApp/EssentialApp/WeakRefVirtualProxy.swift
@@ -21,11 +21,7 @@ extension WeakRefVirtualProxy: ResourceLoadingView where T: ResourceLoadingView 
         object?.display(viewModel)
     }
 }
-extension WeakRefVirtualProxy: FeedImageView where T: FeedImageView, T.Image == UIImage {
-    func display(_ model: FeedImageViewModel<UIImage>) {
-        object?.display(model)
-    }
-}
+
 extension WeakRefVirtualProxy: ResourceErrorView where T: ResourceErrorView {
     func display(_ viewModel: ResourceErrorViewModel) {
         object?.display(viewModel)
