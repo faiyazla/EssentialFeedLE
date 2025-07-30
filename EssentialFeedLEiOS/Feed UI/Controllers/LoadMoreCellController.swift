3291aed2051be2f301928a4f2aa016e3740a2668
faiyazla <fyizahmed@gmail.com>
2025-07-30 20:37:54 +0545

    Add "Load more" cell controller with loading indicator

---
+++ EssentialFeedLEiOS/Feed UI/Controllers/LoadMoreCellController.swift
@@ -0,0 +1,29 @@
+//
+//  LoadMoreCellController.swift
+//  EssentialFeedLE
+//
+//  Created by Faiyaz Ahmed on 30/07/2025.
+//
+
+import UIKit
+import EssentialFeedLE
+
+public class LoadMoreCellController: NSObject, UITableViewDataSource {
+    
+    private let cell = LoadMoreCell()
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        cell
+    }
+}
+
+extension LoadMoreCellController: ResourceLoadingView {
+    public func display(_ viewModel: ResourceLoadingViewModel) {
+        cell.isLoading = viewModel.isLoading
+    }
+}
+
---
+++ EssentialFeedLEiOS/Feed UI/Views/LoadMoreCell.swift
@@ -0,0 +1,36 @@
+//
+//  LoadMoreCell.swift
+//  EssentialFeedLE
+//
+//  Created by Faiyaz Ahmed on 30/07/2025.
+//
+
+import UIKit
+
+public class LoadMoreCell: UITableViewCell {
+    
+    private lazy var spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView(style: .medium)
+        contentView.addSubview(spinner)
+        
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            spinner.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            contentView.heightAnchor.constraint(greaterThanOrEqualToConstant: 40)
+        ])
+        
+        return spinner
+    }()
+    
+    public var isLoading: Bool {
+        get { spinner.isAnimating }
+        set {
+            if newValue {
+                spinner.startAnimating()
+            } else {
+                spinner.stopAnimating()
+            }
+        }
+    }
+}
