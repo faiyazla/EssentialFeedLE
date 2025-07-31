a5db79e6b4e6b8d10f691eae5b1f7fdcd75a4cd1
faiyazla <fyizahmed@gmail.com>
2025-07-31 11:32:19 +0545

    Remove unused code

--- EssentialFeedLE/Feed Presentation/FeedPresenter.swift
+++ EssentialFeedLE/Feed Presentation/FeedPresenter.swift
@@ -17,8 +17,5 @@ public final class FeedPresenter {
                           value: "",
                           comment: "Title for the feed view")
     }
-    
-    public static func map(_ feed: [FeedImage]) -> FeedViewModel {
-        FeedViewModel(feed: feed)
-    }
+
 }
