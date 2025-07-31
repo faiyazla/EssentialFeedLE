f03fe25cca3a6ae6823325989d51c73bef772aeb
faiyazla <fyizahmed@gmail.com>
2025-07-31 12:17:11 +0545

    Prevent load more action while loading more

--- EssentialApp/EssentialApp/CombineHelpers.swift
+++ EssentialApp/EssentialApp/CombineHelpers.swift
@@ -8,6 +8,19 @@
 import Combine
 import EssentialFeedLE
 
+public extension Paginated {
+    var loadMorePublisher: (() -> AnyPublisher<Self, Error>)? {
+        guard let loadMore = loadMore else { return nil }
+        
+        return {
+            Deferred {
+                Future(loadMore)
+            }
+            .eraseToAnyPublisher()
+        }
+    }
+}
+
 public extension HTTPClient {
     
     typealias Publisher = AnyPublisher<(Data, HTTPURLResponse), Error>
