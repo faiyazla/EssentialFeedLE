8c6675ea9b0242f77f98f8b0acd794e36d337b67
faiyazla <fyizahmed@gmail.com>
2025-08-02 12:34:19 +0545

    Prevent loading resources again until a running request completes

--- EssentialApp/EssentialApp/LoadResourcePresenterAdapter.swift
+++ EssentialApp/EssentialApp/LoadResourcePresenterAdapter.swift
@@ -1,5 +1,5 @@
 //
-//  LoadResourcePresenterAdapater.swift
+//  LoadResourcePresenterAdapter.swift
 //  EssentialFeedLEiOS
 //
 //  Created by Faiyaz Ahmed on 21/03/2025.
@@ -13,6 +13,9 @@ import EssentialFeedLEiOS
 final class LoadResourcePresentationAdapter<Resource, View: ResourceView> {
     private let loader: () -> AnyPublisher<Resource, Error>
     private var cancellable: AnyCancellable?
+    
+    private var isLoading = false
+    
     var presenter: LoadResourcePresenter<Resource, View>?
     
     init(loader: @escaping () -> AnyPublisher<Resource, Error>) {
@@ -20,16 +23,22 @@ final class LoadResourcePresentationAdapter<Resource, View: ResourceView> {
     }
     
     func loadResource() {
+        guard !isLoading else { return }
         presenter?.didStartLoading()
+        isLoading = true
         
         cancellable = loader()
             .dispatchOnMainQueue()
+            .handleEvents(receiveCancel: { [weak self] in
+                self?.isLoading = false
+            })
             .sink { [weak self] completion in
             switch completion {
             case .finished: break
             case .failure(let error):
                 self?.presenter?.didFinishLoading(with: error)
             }
+                self?.isLoading = false
         } receiveValue: { [weak self] feed in
             self?.presenter?.didFinishLoading(with: feed)
         }
--- EssentialApp/EssentialAppTests/Helpers/FeedUIIntegrationTests.swift
+++ EssentialApp/EssentialAppTests/Helpers/FeedUIIntegrationTests.swift
@@ -45,8 +45,13 @@ class FeedUIIntegrationTests: XCTestCase {
         XCTAssertEqual(loader.loadFeedCallCount, 1, "Expected a loading request once view is loaded")
         
         sut.simulateUserInitiatedReload()
+        XCTAssertEqual(loader.loadFeedCallCount, 1, "Expected no request until previous completes")
+        loader.completeFeedLoading(at: 0)
+        
+        sut.simulateUserInitiatedReload()
         XCTAssertEqual(loader.loadFeedCallCount, 2, "Expected another loading request once user initiates a reload")
         
+        loader.completeFeedLoading(at: 1)
         sut.simulateUserInitiatedReload()
         XCTAssertEqual(loader.loadFeedCallCount, 3, "Expected yet another loading request once user initiates another reload")
     }
@@ -290,6 +295,27 @@ class FeedUIIntegrationTests: XCTestCase {
         
         sut.simulateFeedImageViewNotVisible(at: 1)
         XCTAssertEqual(loader.cancelledImageURLs, [image0.url, image1.url], "Expected two cancelled image URL requests once second image is also not visible anymore")
+    }
+    
+    func test_feedImageView_doesNotLoadImageAgainUntilPreviousRequestCompletes() {
+        let image = makeImage(url: URL(string: "http://url-0.com")!)
+        let (sut, loader) = makeSUT()
+        sut.simulateAppearance()
+        loader.completeFeedLoading(with: [image])
+        
+        sut.simulateFeedImageViewNearVisible(at: 0)
+        XCTAssertEqual(loader.loadedImageURLs, [image.url], "Expected first request when near visible")
+        
+        sut.simulateFeedImageViewVisible(at: 0)
+        XCTAssertEqual(loader.loadedImageURLs, [image.url], "Expected no request until previous completes")
+        
+        loader.completeImageLoading(at: 0)
+        sut.simulateFeedImageViewVisible(at: 0)
+        XCTAssertEqual(loader.loadedImageURLs, [image.url, image.url], "Expected second request when visible after previous complete")
+        
+        sut.simulateFeedImageViewNotVisible(at: 0)
+        sut.simulateFeedImageViewVisible(at: 0)
+        XCTAssertEqual(loader.loadedImageURLs, [image.url, image.url, image.url], "Expected third request when visible after canceling previous complete")
     }
     
     func test_feedImageViewLoadingIndicator_isVisibleWhileLoadingImage() {
