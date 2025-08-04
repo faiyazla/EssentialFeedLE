84f6aeb52bf18cbda30561a557357e31cec74a88
faiyazla <fyizahmed@gmail.com>
2025-08-04 16:12:58 +0545

    Make in memory FeedImageDataStore implementation sync

--- EssentialApp/EssentialAppTests/Helpers/InMemoryFeedStore.swift
+++ EssentialApp/EssentialAppTests/Helpers/InMemoryFeedStore.swift
@@ -8,14 +8,16 @@
 import Foundation
 import EssentialFeedLE
 
-class InMemoryFeedStore: FeedStore, FeedImageDataStore {
+class InMemoryFeedStore {
     private(set) var feedCache: CachedFeed?
     private var feedImageDataCache: [URL: Data] = [:]
     
     private init(feedCache: CachedFeed? = nil) {
-         self.feedCache = feedCache
-     }
-    
+        self.feedCache = feedCache
+    }
+}
+
+extension InMemoryFeedStore: FeedStore {
     func deleteCachedFeed(completion: @escaping FeedStore.DeletionCompletion) {
         feedCache = nil
         completion(.success(()))
@@ -29,25 +31,28 @@ class InMemoryFeedStore: FeedStore, FeedImageDataStore {
     func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
         completion(.success(feedCache))
     }
-    
-    func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
+}
+
+extension InMemoryFeedStore: FeedImageDataStore {
+    func insert(_ data: Data, for url: URL) throws {
         feedImageDataCache[url] = data
-        completion(.success(()))
     }
     
-    func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
-        completion(.success(feedImageDataCache[url]))
+    func retrieve(dataForURL url: URL) throws -> Data? {
+        feedImageDataCache[url]
     }
-    
+}
+
+extension InMemoryFeedStore {
     static var empty: InMemoryFeedStore {
         InMemoryFeedStore()
     }
     
     static var withExpiredFeedCache: InMemoryFeedStore {
-         InMemoryFeedStore(feedCache: CachedFeed(feed: [], timestamp: Date.distantPast))
-     }
-
-     static var withNonExpiredFeedCache: InMemoryFeedStore {
-         InMemoryFeedStore(feedCache: CachedFeed(feed: [], timestamp: Date()))
-     }
+        InMemoryFeedStore(feedCache: CachedFeed(feed: [], timestamp: Date.distantPast))
+    }
+    
+    static var withNonExpiredFeedCache: InMemoryFeedStore {
+        InMemoryFeedStore(feedCache: CachedFeed(feed: [], timestamp: Date()))
+    }
 }
