31c247a5ff9405e1416c0dab6c6b99c76f484163
faiyazla <fyizahmed@gmail.com>
2025-08-04 12:17:18 +0545

    Load images synchronously

--- EssentialFeedLETests/Feed Cache/LoadFeedImageDataFromCacheUseCacheTests.swift
+++ EssentialFeedLETests/Feed Cache/LoadFeedImageDataFromCacheUseCacheTests.swift
@@ -52,34 +52,6 @@ class LoadFeedImageDataFromCacheUseCacheTests: XCTestCase {
         })
     }
     
-    func test_loadImageDataFromURL_doesNotDeliverResultAfterCancellingTask() {
-        let (sut, store) = makeSUT()
-        let foundData = anyData()
-        
-        var received = [FeedImageDataLoader.Result]()
-        let task = sut.loadImageData(from: anyURL()) { received.append($0) }
-        task.cancel()
-        
-        store.completeRetrieval(with: foundData)
-        store.completeRetrieval(with: .none)
-        store.completeRetrieval(with: anyNSError())
-        
-        XCTAssertTrue(received.isEmpty, "Expected no received results after cancelling task")
-    }
-    
-    func test_loadImageDataFromURL_doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated() {
-        let store = FeedImageDataStoreSpy()
-        var sut: LocalFeedImageDataLoader? = LocalFeedImageDataLoader(store: store)
-        
-        var received = [FeedImageDataLoader.Result]()
-        _ = sut?.loadImageData(from: anyURL()) { received.append($0) }
-        
-        sut = nil
-        store.completeRetrieval(with: anyData())
-        
-        XCTAssertTrue(received.isEmpty, "Expected no received results after instance has been deallocated")
-    }
-    
     //MARK: Helpers
     
     private func makeSUT(currentData: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (LocalFeedImageDataLoader, FeedImageDataStoreSpy) {
@@ -104,7 +76,8 @@ class LoadFeedImageDataFromCacheUseCacheTests: XCTestCase {
     
     private func expect(_ sut: LocalFeedImageDataLoader, toCompleteWith expectedResult: FeedImageDataLoader.Result, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
         let exp = expectation(description: "Wait for load completion")
-        
+        action()
+
         _ = sut.loadImageData(from: anyURL()) { receivedResult in
             switch (receivedResult, expectedResult) {
             case let (.success(receivedData), .success(expectedData)):
@@ -121,7 +94,6 @@ class LoadFeedImageDataFromCacheUseCacheTests: XCTestCase {
             exp.fulfill()
         }
         
-        action()
         wait(for: [exp], timeout: 1.0)
     }
 }
