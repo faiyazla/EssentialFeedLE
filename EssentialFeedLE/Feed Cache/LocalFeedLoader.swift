deff41124550156ab06eac3d270bbd400f0c6d0a
faiyazla <fyizahmed@gmail.com>
2025-08-05 16:08:46 +0545

    Make LocalFeedLoader.validateCache sync

--- EssentialFeedLE/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeedLE/Feed Cache/LocalFeedLoader.swift
@@ -33,21 +33,17 @@ extension LocalFeedLoader {
     }
 }
 
-extension LocalFeedLoader {
-    public typealias ValidationResult = Result<Void, Error>
-    
+extension LocalFeedLoader {    
     private struct InvalidCache: Error {}
     
-    public func validateCache(completion: @escaping (ValidationResult) -> Void) {
-        completion(ValidationResult {
-             do {
-                 if let cache = try store.retrieve(), !FeedCachePolicy.validate(cache.timestamp, against: currentDate()) {
-                     throw InvalidCache()
-                 }
-             } catch {
-                 try store.deleteCachedFeed()
-             }
-         })
+    public func validateCache() throws {
+        do {
+            if let cache = try store.retrieve(), !FeedCachePolicy.validate(cache.timestamp, against: currentDate()) {
+                throw InvalidCache()
+            }
+        } catch {
+            try store.deleteCachedFeed()
+        }
     }
 }
 
